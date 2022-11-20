688f38ae1be3ea8c2b6224af3a8d7ca97ecd485d
Pablo Baldazo <akibal1960@gmail.com>
2022-11-20 14:03:44 +0100

    Ponemos sonidos a los botones.

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		PrincipalFrame frame = new PrincipalFrame();
 		EventsController control = new EventsController(frame);
+		control.playSound("sounds/BSO_Jpark.wav");
 		frame.controller(control);
 
 	}
