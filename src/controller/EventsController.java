f7975f31e574ceb088807444612b35e4324c6bb2
Pablo Baldazo <akibal1960@gmail.com>
2022-11-20 12:12:52 +0100

    Volvemos a subir método para sonidos.

--- src/controller/EventsController.java
+++ src/controller/EventsController.java
@@ -2,7 +2,14 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
 
 import view.PrincipalFrame;
@@ -113,6 +120,18 @@ public class EventsController implements ActionListener{
 		
 		public double round(double num) {
             return Math.round(num * 100.0)/100.0;
+        }
+		//función para agregar el sonido a los eventos
+        public void PlaySound(String sound){
+            try {
+
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(sound).getAbsoluteFile());
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+            }catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+                System.out.println("Error al reproducir el sonido.");
+            }
         }
 			
 }
